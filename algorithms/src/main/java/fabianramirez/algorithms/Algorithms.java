8c172c8643accf6e15cb10cff561c93e1b310e40
fabian ramirez <fabian_alex73@hotmail.com>
2023-05-17 22:32:07 -0600

    first commit

---
+++ algorithms/src/main/java/fabianramirez/algorithms/Algorithms.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package fabianramirez.algorithms;
+
+import views.Menu;
+
+/**
+ *
+ * @author fabia
+ */
+public class Algorithms {
+
+    public static void main(String[] args) {
+       Menu menu= new Menu();
+       menu.setVisible(true);
+       menu.setLocationRelativeTo(null);
+       menu.setResizable(false);
+    }
+}
