327789b0ad9dafc7d18efbfca9b43835bf441b93
fabian ramirez <fabian_alex73@hotmail.com>
2023-05-18 20:21:28 -0600

    second commit

---
+++ algorithms/src/main/java/Controller/Palindrome.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author fabia
+ */
+public class Palindrome {
+    
+    public static String pb, texto, palabra, palabrab;
+    public static void main(String[] args) 
+    {
+     palabra=JOptionPane.showInputDialog("Ingresa una palabra: ");
+     for (int i = 0; i < palabra.length(); i++)
+     {
+     }
+     }
+        
+}
