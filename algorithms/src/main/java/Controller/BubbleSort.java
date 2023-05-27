224af7b1f77f3f4159ed9220ffb6d23d461cabd4
fabian ramirez <fabian_alex73@hotmail.com>
2023-05-26 22:09:08 -0600

    add bubble method

--- algorithms/src/main/java/Controller/BubbleSort.java
+++ algorithms/src/main/java/Controller/BubbleSort.java
@@ -9,8 +9,48 @@ package Controller;
  * @author fabia
  */
 public class BubbleSort {
-    public void BubbleSortllamar(){
-        System.out.println("♪♪♪");
+
+    public void BubbleSortllamar() {
+
+        int[] arr = {5, 2, 8, 12, 1, 6}; // Array de ejemplo
+
+        System.out.println("Array antes del ordenamiento:");
+        printArray(arr);
+
+        bubbleSort(arr);
+
+        System.out.println("Array después del ordenamiento:");
+        printArray(arr);
     }
-    
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Intercambiar arr[j] y arr[j + 1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            // Si no hubo intercambios en esta iteración, el array está ordenado
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
 }
