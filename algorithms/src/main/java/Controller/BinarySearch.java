cd7b4d49ae29a4da02534215aa93b73ed5d07d78
fabian ramirez <fabian_alex73@hotmail.com>
2023-06-01 23:43:05 -0600

    add method

--- algorithms/src/main/java/Controller/BinarySearch.java
+++ algorithms/src/main/java/Controller/BinarySearch.java
@@ -9,7 +9,40 @@ package Controller;
  * @author fabia
  */
 public class BinarySearch {
-    public void Binaryllamar(){
-        System.out.println("Binary Search");
+
+    int binarySearch(int arr[], int target) {
+        int left = 0, right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            // Si el elemento está presente en el medio
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            // Si el elemento es mayor, ignora la mitad izquierda del arreglo
+            if (arr[mid] < target) {
+                left = mid + 1;
+            } // Si el elemento es menor, ignora la mitad derecha del arreglo
+            else {
+                right = mid - 1;
+            }
+        }
+
+        // Si el elemento no está presente en el arreglo
+        return -1;
+    }
+
+    public void Binaryllamar() {
+        BinarySearch binarySearch = new BinarySearch();
+        int arr[] = {2, 4, 6, 8, 10, 12, 14, 16};
+        int target = 10;
+        int result = binarySearch.binarySearch(arr, target);
+        if (result == -1) {
+            System.out.println("El elemento no se encuentra en el arreglo.");
+        } else {
+            System.out.println("El elemento se encuentra en el índice " + result + ".");
+        }
+
     }
 }
